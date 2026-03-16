1e15f6e00a8c9484b6c92d4f551ffa7518fb800c
Aaron Wright <aaron@sikosoft.com>
2026-03-16 18:51:11 +0100

    add donation button in credit-bar

--- src/components/credit-bar/credit-bar.ts
+++ src/components/credit-bar/credit-bar.ts
@@ -1,5 +1,7 @@
 import { css, html, LitElement, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
+import { translate } from '@/lib/Localization';
+import '@ss/ui/components/ss-button';
 
 @customElement('credit-bar')
 export class CreditBar extends LitElement {
@@ -29,7 +31,15 @@ export class CreditBar extends LitElement {
       }
     }
 
+    ss-button {
+      display: inline-block;
+      vertical-align: middle;
+      margin-right: 1rem;
+    }
+
     .github-icon {
+      display: inline-block;
+      vertical-align: middle;
       width: 2rem;
       height: 2rem;
       border: 0;
@@ -40,7 +50,31 @@ export class CreditBar extends LitElement {
         opacity: 1;
       }
     }
+
+    .donation {
+      color: #000;
+      text-align: left;
+
+      .message {
+        margin-bottom: 1rem;
+      }
+
+      ul,
+      li {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        text-align: center;
+      }
+
+      .currency {
+        font-weight: bold;
+      }
+    }
   `;
+
+  @state()
+  private donateIsOpen = false;
 
   render(): TemplateResult {
     return html`
@@ -49,11 +83,42 @@ export class CreditBar extends LitElement {
           Created with ♥ by <a href="https://sikosoft.com">Aaron Wright</a>
         </div>
         <div class="right">
+          <ss-button @click=${() => (this.donateIsOpen = !this.donateIsOpen)}>
+            ${translate('donate')}
+          </ss-button>
           <a href="https://github.com/SikoSoft/k4"
             ><img src="./GitHub_Invertocat_White.png" class="github-icon"
           /></a>
         </div>
       </div>
+
+      <pop-up
+        closeButton
+        closeOnEsc
+        closeOnOutsideClick
+        ?open=${this.donateIsOpen}
+        @pop-up-closed=${() => (this.donateIsOpen = false)}
+      >
+        <div class="donation">
+          <div class="message">${translate('donateMessage')}</div>
+          <ul>
+            <li>
+              <span class="currency">BTC</span>:
+              <code>bc1qqp9auka4ur7kljzqyuwcx7rrksp4n54eaqlqzy</code>
+            </li>
+            <li>
+              <span class="currency">BCH</span>:
+              <code
+                >bitcoincash:qqnpkmgge90qh5xs38dtzfhrx8mp5nqk8gxazfesn0</code
+              >
+            </li>
+            <li>
+              <span class="currency">DOGE</span>:
+              <code>DKprWAKptzHngpRSNRY9W6Tiu24WJkFBju</code>
+            </li>
+          </ul>
+        </div></pop-up
+      >
     `;
   }
 }
